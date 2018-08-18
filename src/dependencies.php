3eecafbe3b0bdded7a6f85b6684145568397de10
Guilherme Scaranse <guiscaranse@gmail.com>
2018-08-17 21:36:09 -0300

    Adicionado twig

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,9 +4,14 @@
 $container = $app->getContainer();
 
 // view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+$container['view'] = function ($container) {
+    $settings = $container->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path']);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+    return $view;
 };
 
 // monolog
