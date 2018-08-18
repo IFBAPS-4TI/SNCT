5afc5333c6124e82cced8e714d8ff37541f05e33
Guilherme Scaranse <guiscaranse@gmail.com>
2018-08-17 21:42:11 -0300

    Modificado algumas coisas para o Twig funcionar e adicionado cache
    Esqueci :P

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,14 +3,16 @@
 
 $container = $app->getContainer();
 
-// view renderer
+// Register component on container
 $container['view'] = function ($container) {
-    $settings = $container->get('settings')['renderer'];
-    $view = new \Slim\Views\Twig($settings['template_path']);
-    $view->addExtension(new \Slim\Views\TwigExtension(
-        $container['router'],
-        $container['request']->getUri()
-    ));
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' =>  __DIR__ . '/../cache/'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
     return $view;
 };
 
