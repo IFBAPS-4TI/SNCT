3132e713b962c534508562bc9236166dcae75707
Guilherme Scaranse <guiscaranse@gmail.com>
2018-08-23 19:19:02 -0300

    Adicionado painel de usuário

--- src/routes.php
+++ src/routes.php
@@ -13,10 +13,11 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->view->render($response, 'home/index.html', $args);
 });
 
-$app->get('/old', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->view->render($response, 'old/index.html', $args);
+$app->group('/panel', function() {
+    $this->get('/login', function ($request, $response, $args) {
+        return $this->view->render($response, 'panel/login.html', $args);
+    });
+    $this->get('/dash', function ($request, $response, $args) {
+        return $this->view->render($response, 'panel/panel.html', $args);
+    });
 });
