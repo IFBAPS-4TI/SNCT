81c3d0379862b2787a4a63d9300504d0b81d6a43
Guilherme Scaranse <guiscaranse@gmail.com>
2018-08-23 18:35:18 -0300

    Atualizada data

--- public/js/app.js
+++ public/js/app.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse("2018-10-16T00:00:00"));
+var deadline = new Date(Date.parse("2018-10-15T00:00:00"));
 initializeClock('clockdiv', deadline);
