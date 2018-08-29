e0bbbefd941ca0b44347b849fb2d980de2389699
Guilherme Scaranse <guiscaranse@gmail.com>
2018-08-29 18:13:46 -0300

    Alterado forma que as configurações do SlimSession são dadas

--- src/middleware.php
+++ src/middleware.php
@@ -2,8 +2,13 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$settings = $container->get('settings');
+/*
+ * SlimSession
+ */
 $app->add(new \Slim\Middleware\Session([
-    'name' => 'snctcontrol',
-    'autorefresh' => true,
-    'lifetime' => '1 week'
+    'name' => $settings['sessions']['name'],
+    'autorefresh' => $settings['sessions']['autorefresh'],
+    'lifetime' => $settings['sessions']['lifetime']
 ]));
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,10 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        'sessions' =>[
+          'name' => 'snctcontrol',
+          'autorefresh' => true,
+          'lifetime' => '1 week'
+        ],
     ],
 ];
