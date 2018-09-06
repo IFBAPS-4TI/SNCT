aeec41ecd661430e46c40fee450cea8cdb4cb77a
Guilherme Scaranse <guiscaranse@gmail.com>
2018-09-06 10:10:47 -0300

    Adicionado variável de jwt_key

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
-
+$jwt_securekey = $_SERVER["jwt_key"]; # Definir jwt_key no ambiente
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
