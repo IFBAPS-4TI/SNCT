7b216dfa01ebca0850b62a847e7b89b48f25fd29
Guilherme Scaranse <guiscaranse@gmail.com>
2018-09-08 17:02:33 -0300

    Adicionado modelo de usuário

--- src/models/Usuario.php
+++ src/models/Usuario.php
@@ -6,5 +6,118 @@ namespace Models;
 
 class Usuario
 {
+    private $id;
+    private $nome;
+    private $email;
+    private $nascimento;
+    private $cpf;
+    private $senha;
 
+    function better_crypt($input, $rounds = 7)
+    {
+        $salt = "";
+        $salt_chars = array_merge(range('A', 'Z'), range('a', 'z'), range(0, 9));
+        for ($i = 0; $i < 22; $i++) {
+            $salt .= $salt_chars[array_rand($salt_chars)];
+        }
+        return crypt($input, sprintf('$2y$%02d$', $rounds) . $salt);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNascimento()
+    {
+        return $this->nascimento;
+    }
+
+    /**
+     * @param mixed $nascimento
+     */
+    public function setNascimento($nascimento)
+    {
+        $this->nascimento = $nascimento;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * @param mixed $senha
+     */
+    public function setSenha($senha)
+    {
+        $senha_hash = better_crypt($senha);
+        unset($senha);
+        $this->senha = $senha_hash;
+    }
 }
