8a82618cc083f1b9d087b71839fc57cbb9b73c83
Guilherme Scaranse <guiscaranse@gmail.com>
2018-09-08 16:53:49 -0300

    Adicionado indexar modelos

--- src/addons.php
+++ src/addons.php
@@ -2,6 +2,9 @@
 /*
  * NÃO EDITAR ESSE ARQUIVO. Adicione os arquivos na pasta "controllers"
  */
+foreach (glob(__DIR__ . '/../src/models/*.php') as $filename) {
+    include $filename;
+}
 foreach (glob(__DIR__ . '/../src/controllers/*.php') as $filename) {
     include $filename;
 }
